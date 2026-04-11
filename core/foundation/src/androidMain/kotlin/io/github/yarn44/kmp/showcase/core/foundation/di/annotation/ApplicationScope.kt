e777221f87719e00517225566565d519fb415a95
mitsuharu yoshiyama <m.yoshiyama@gmail.com>
2026-04-11 14:39:30 +0900

    chore(core-foundation): remove unused Hilt dispatcher scaffolding

--- core/foundation/src/androidMain/kotlin/io/github/yarn44/kmp/showcase/core/foundation/di/annotation/ApplicationScope.kt
+++
@@ -1,7 +0,0 @@
-package io.github.yarn44.kmp.showcase.core.foundation.di.annotation
-
-import javax.inject.Qualifier
-
-@Qualifier
-@Retention(AnnotationRetention.BINARY)
-annotation class ApplicationScope
