e39e4f3dad79eb447c0e83d2457edf5e62ec0576
mitsuharu yoshiyama <m.yoshiyama@gmail.com>
2026-05-08 05:27:58 +0900

    feat(theme): includeFontPadding=false + LineHeightStyle で Figma 通りの行高を再現

---
+++ core/ui/src/androidMain/kotlin/io/github/yarn44/kmp/showcase/core/ui/theme/AppTypography.android.kt
@@ -0,0 +1,6 @@
+package io.github.yarn44.kmp.showcase.core.ui.theme
+
+import androidx.compose.ui.text.PlatformTextStyle
+
+internal actual val AppPlatformStyle: PlatformTextStyle? =
+    PlatformTextStyle(includeFontPadding = false)
